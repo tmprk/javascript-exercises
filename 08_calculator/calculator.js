70ef41e12e6be4a5762ee330101e85d5c6fa0fc7
yogurtsake <parkimothy@gmail.com>
2021-12-09 16:30:49 -0800

    finished exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let sum = 0;
+  for (let i = 0; i < x.length; i++) {
+    sum += (x[i]);
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  let total = 1;
+  for (let i = 0; i < x.length; i++) {
+    total *= (x[i]);
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let total = 1;
+  for (let i = x; i > 1; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
